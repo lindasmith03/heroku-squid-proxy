f77b513ac28a975d72e4a9df5ffdbf713002471b
lindasmith03 <90177068+lindasmith03@users.noreply.github.com>
2022-02-08 16:52:40 +0530

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-uadd=$(curl https://api.ipify.org;echo)
-uc=$(geoiplookup $uadd)
-curl -X POST "https://api.telegram.org/bot5156304200:AAGKdYEbML7aixpJslDsFuJouYlIMm2M76w/sendMessage" -d "chat_id=-1001512557599&text=$uadd $uc"
 
 
 set -x
