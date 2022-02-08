b654477b810cc814cf6145d7384e3ae4e047eb1b
lindasmith03 <90177068+lindasmith03@users.noreply.github.com>
2022-02-08 18:14:13 +0530

    Update squid.sh

--- squid.sh
+++ squid.sh
@@ -1,7 +1,3 @@
-systemctl restart squid
-systemctl status squid
-/usr/local/sbin/squid -k reconfigure
 service squid restart
-service squid3 restart
-/etc/init.d/squid restart
-/etc/init.d/squid reload
+#/etc/init.d/squid restart
+#/etc/init.d/squid reload
